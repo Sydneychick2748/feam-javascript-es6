38021ef134a2069ac22cdf637d8ef3501101096b
ana <ana@anniecannons.com>
2023-11-16 13:40:02 -0800

    commited ternary

--- ternaryStatements.js
+++ ternaryStatements.js
@@ -3,11 +3,22 @@
 /*Make sure to check that everything works as expected in the console before turning in your work */
 
 /*Exercise 1*/
+let weather = "rain";
 if(weather === "rain") {
     console.log("Please wear rain boots")
 }
 
+
+
 //refactor below
+let weather1 = "rain";
+const getWeather = () => { 
+  weather1 === "rain" ? console.log("Please wear rain boots") : console.log("No need to wear rain boots")
+
+}
+getWeather()
+
+
 
 
 
@@ -15,10 +26,22 @@ if(weather === "rain") {
 
 
 /*Exercise 2*/
+let student = true 
+let loggedIn = true
 if(student == true && loggedIn == true) {
     console.log("This is where we would write the code to show the entire site")
 }
+
 //refactor below
+let student1 = true
+let loggedIn1 = true
+
+const getInformation = ()=>{
+  student1 == true && loggedIn1 == true ? console.log("This is where we would write the code to show the entire site"): console.log("do nothing ")
+}
+
+getInformation()
+
 
 
 
@@ -35,10 +58,24 @@ if (cash < 10) {
 }else if (cash > 20) {
   lunch = "lobster rolls";
 } 
-
+console.log(lunch)
 //refactor below
 
 
+let cash1 = 18;
+let lunch1 = "";
 
 
+const getLunch = ()=>{
 
+  cash1< 10
+  ?(lunch1 = "sandwich")
+  :cash1 <= 20 && cash1 > 11
+  ?(lunch1= "fancy sandwich")
+  :cash1 > 20
+  ?(lunch1 = "lobster rolls")
+  :"no sandwich";
+   
+}
+getLunch()
+console.log(lunch1)
